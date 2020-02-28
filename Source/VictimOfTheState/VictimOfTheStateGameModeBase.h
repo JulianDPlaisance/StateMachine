62551093a2b63c2d94b55b9c3599ba711bc117f8
Julian Dane Plaisance <jplais8@lsu.edu>
2020-02-28 15:19:18 -0600

    base state machine

---
+++ Source/VictimOfTheState/VictimOfTheStateGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VictimOfTheStateGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VICTIMOFTHESTATE_API AVictimOfTheStateGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
