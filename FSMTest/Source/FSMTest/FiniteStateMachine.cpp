2aa7c4dad057f8c56a865c56e0ef2192f5b8d15e
Julian Dane Plaisance <jplais8@lsu.edu>
2020-05-07 18:16:37 -0500

    added a changes proposal

--- FSMTest/Source/FSMTest/FiniteStateMachine.cpp
+++ FSMTest/Source/FSMTest/FiniteStateMachine.cpp
@@ -4,7 +4,6 @@
 #include "FiniteStateMachine.h"
 #include "SleepingState.h"
 #include "EatingState.h"
-//class UEatingState;
 
 // Sets default values for this component's properties
 UFiniteStateMachine::UFiniteStateMachine()
